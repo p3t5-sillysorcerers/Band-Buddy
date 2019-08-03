9d43da33e18843d0f807257b023a235bb306eb78
Phil Dugliss <phillipdugliss@gmail.com>
2019-08-03 11:24:07 -0500

    Create Page added
    -create Profile route added
    -create Profile page added

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
 import Profile from './pages/profile';
 import Login from './pages/login';
 import ActiveUsers from './pages/activeUsers';
+import Create from './pages/createProfile';
 import NotFound from './pages/notFound';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
@@ -16,6 +17,7 @@ class App extends Component {
         <div>
           <Switch>
           <Route exact path="/" component={Login}/>
+          <Route exact path="/create" component={Create}/>
           <Route exact path="/profile" component={Profile}/>
           <Route exact path="/activeusers" component={ActiveUsers}/>
           <Route component = {NotFound}/>
---
+++ client/src/pages/createProfile.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+
+class Create extends Component{
+
+    render(){
+        return(
+
+            <div className="createPage">
+                <h1>createPage</h1>
+            </div>
+
+
+
+        )
+    }
+}
+
+export default Create;
