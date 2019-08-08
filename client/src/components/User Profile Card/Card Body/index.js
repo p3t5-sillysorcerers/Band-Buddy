da2320ef61ab80f1e760b32816ff34e4a3df74c6
Phil Dugliss <phillipdugliss@gmail.com>
2019-08-07 21:39:27 -0500

    Active Users Information Displaying
    - Data access from database
    -returning data to active users card
    -card content div added
    -props passed to card content div
    -card body map function to display all data

--- client/src/components/User Profile Card/Card Body/index.js
+++ client/src/components/User Profile Card/Card Body/index.js
@@ -1,30 +1,44 @@
-import React  from "react";
+import React from "react";
 import './style.css'
 
 const user = {
     userInfo: {
         image: "https://randomuser.me/api/portraits/women/31.jpg",
-        name: "Jamie Winehouse",
-        info: "I'm a guitarist and vocalist looking for a band.",
-        location: "Wicker Park"
     }
 }
 
-function CardBody() {
+
+class CardBody extends React.Component {
+
+render() {
     return (
         <div className="card">
             <div className="img-container">
                 <img src={user.userInfo.image} className="card-image" alt="..."></img>
             </div>
-            <div className="card-body">
-                <h3 className="name">{user.userInfo.name}</h3>
-                <p className="title">{user.userInfo.info}</p>
-                <p className="card-text">{user.userInfo.location}</p>
+            <div className="contentDiv">
+                <ul>
+                    <li>
+                        <strong>Name:</strong> {this.props.name}
+                    </li>
+                    <li>
+                        <strong>User Name:</strong> {this.props.username}
+                    </li>
+                    <li>
+                        <strong>From:</strong> {this.props.location}
+                    </li>
+                    <li>
+                        <strong>Plays:</strong> {this.props.instrument}
+                    </li>
+                </ul>
+            </div>
+            <div className="buttonDiv">
                 <a href="#" className="btn btn-primary">Contact Me</a>
                 <button type="button" class="btn btn-success">Available</button>
             </div>
-        </div>
+    </div>
+
     );
 }
-
+}
 export default CardBody;
