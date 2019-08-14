2f4d414dd4a19c2ce9ca2b8c5a322688f8b7ec92
Phil Dugliss <phillipdugliss@gmail.com>
2019-08-14 11:27:15 -0500

    Began adding authentication
    -Added authentication fields to profile Schema
    -Added methods to profile schema for setting and validating password
    - added packages for passport and jwt
    -added express-session and cors to server js

--- models/profiles.js
+++ models/profiles.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
 const Schema = mongoose.Schema;
 
 const profileSchema = new Schema({
   name: { type: String, required: true },
-  userName: { type: String, required: true },
+  hash: { type: String, required: true},
+  salt: {type: String, required: true},
+  userName: { type: String, required: true, unique: true },
   instrument: { type: String },
   skillLevel: { type: String },
   musicGenres:[],
@@ -13,6 +17,39 @@ const profileSchema = new Schema({
   image: { data: Buffer, contentType: String  },
   updated: { type: Date, default: Date.now }
 });
+
+
+profileSchema.methods.setPassword = function(password) {
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+};
+
+profileSchema.methods.validatePassword = function(password) {
+  const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+  return this.hash === hash;
+};
+
+profileSchema.methods.generateJWT = function() {
+  const today = new Date();
+  const expirationDate = new Date(today);
+  expirationDate.setDate(today.getDate() + 60);
+
+  return jwt.sign({
+    userName: this.userName,
+    id: this._id,
+    exp: parseInt(expirationDate.getTime() / 1000, 10),
+  }, '67e1f543-e640-4bc1-8279-9f4f10da1f35');
+}
+
+profileSchema.methods.toAuthJSON = function() {
+  return {
+    _id: this._id,
+    userName: this.userName,
+    token: this.generateJWT(),
+  };
+};
+
+
 
 const Profiles = mongoose.model("Profiles", profileSchema);
 
--- server.js
+++ server.js
@@ -3,10 +3,18 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+const errorHandler = require('errorhandler');
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
+app.use(session({ secret: '67e1f543-e640-4bc1-8279-9f4f10da1f35', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -23,9 +31,11 @@ mongoose.connect(
   "mongodb://heroku_bdq0hmmf:ufc9jh2h6453oc1qfa50i7ea5m@ds259347.mlab.com:59347/heroku_bdq0hmmf"
 );
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+app.use('/*', express.static('client/build'))
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
